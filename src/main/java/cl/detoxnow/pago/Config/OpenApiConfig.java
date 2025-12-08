5f6fcf1c7772abdda534388b6cc951d7d1205e48
scarletjara <scarlet.jara@hotmail.com>
2025-12-08 20:28:33 -0300

    Puerto aleatorio

---
+++ src/main/java/cl/detoxnow/pago/Config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package cl.detoxnow.pago.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Api Pago DetoxNow")
+                .version("1.0.0")
+                .description("Documentación de la API desarrollada en Spring Boot")
+                .contact(new Contact()
+                    .name("Equipo DetoxNow")
+                    .email("sc.jaraf@duocuc.cl"))
+            );
+    }
+}
